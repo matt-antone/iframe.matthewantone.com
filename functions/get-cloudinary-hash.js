6950cc8a481155fce6aade26184a794c839f6b40
Matt Antone <matt.antone@gmail.com>
2020-03-17 16:37:49 -0700

    updated to send hash

--- functions/get-cloudinary-hash.js
+++ functions/get-cloudinary-hash.js
@@ -1,4 +1,15 @@
+var crypto = require('crypto');
 const checkAuth = require('./utils/auth')
+const cloud = "bugo"
+const email = "accounts@bugo.io"
+// const apiKey = "987834768892112"
+const apiSecret = "9IIeBG_45eV0xhvFapBOGzmCzBs"
+const date = new Date();
+const timestamp = date.getTime();
+const paramters = `cloud_name=${cloud}&timestamp=${timestamp}&username=${email}${apiSecret}`
+let hash = crypto.createHash('sha256');
+hash.update(paramters)
+
 
 exports.handler = (event, context, callback) => {
   // Use the event data auth header to verify
@@ -8,7 +19,7 @@ exports.handler = (event, context, callback) => {
     return callback(null, {
       statusCode: 200,
       body: JSON.stringify({
-        data: true
+        data: hash.digest('hex')
       })
     })
   }).catch((error) => {

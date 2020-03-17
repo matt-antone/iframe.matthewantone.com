4bc72bb14ae6af920adea2563a0ee16771f2e00c
Matt Antone <matt.antone@gmail.com>
2020-03-17 13:02:04 -0700

    added handler

--- functions/get-cloudinary-hash.js
+++ functions/get-cloudinary-hash.js
@@ -1,15 +1,17 @@
-// exports.handler = (event, context, callback) => {
-//   const {identity, user} = context.clientContext;
-//   // return JSON.stringify([identity,user])
-//   callback(null, {
-//     statusCode: 200,
-//     body: JSON.stringify([identity,user])
-//   });
-// }
+exports.handler = (event, context, callback) => {
+  const {identity, user} = context.clientContext;
+  const loggedin = checkAuth(context)
+
+  // return JSON.stringify([identity,user])
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify(loggedin)
+  });
+}
 
 
 /* Check context for user */
-exports.handler = function checkAuth(context) {
+module.exports = function checkAuth(context) {
   return new Promise((resolve, reject) => {
     // Reading the context.clientContext will give us the current user
     const user = context.clientContext && context.clientContext.user

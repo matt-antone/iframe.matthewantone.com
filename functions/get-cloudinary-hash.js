dda88ab56df50a3297406012016b059fa1708e7b
Matt Antone <matt.antone@gmail.com>
2020-03-17 12:55:57 -0700

    updated function

--- functions/get-cloudinary-hash.js
+++ functions/get-cloudinary-hash.js
@@ -1,8 +1,23 @@
-exports.handler = (event, context, callback) => {
-  const {identity, user} = context.clientContext;
-  // return JSON.stringify([identity,user])
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify([identity,user])
-  });
+// exports.handler = (event, context, callback) => {
+//   const {identity, user} = context.clientContext;
+//   // return JSON.stringify([identity,user])
+//   callback(null, {
+//     statusCode: 200,
+//     body: JSON.stringify([identity,user])
+//   });
+// }
+
+
+/* Check context for user */
+module.exports = function checkAuth(context) {
+  return new Promise((resolve, reject) => {
+    // Reading the context.clientContext will give us the current user
+    const user = context.clientContext && context.clientContext.user
+    if (!user) {
+      console.log('No claims! Begone!')
+      return reject(new Error('No user claims'))
+    }
+    console.log('user', user)
+    return resolve(user)
+  })
 }

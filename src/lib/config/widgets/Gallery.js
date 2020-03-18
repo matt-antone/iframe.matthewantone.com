83b334808e7c7c3d0fd1c0dcdeabe5c08db0ac0d
Matt Antone <matt.antone@gmail.com>
2020-03-17 19:25:32 -0700

    update

--- src/lib/config/widgets/Gallery.js
+++ src/lib/config/widgets/Gallery.js
@@ -45,13 +45,13 @@ export default class Gallery extends Component {
     .then((result)=>{
       console.log(result);
       window.ML = window.cloudinary.createMediaLibrary({
-        api_key: result.key,
+        api_key: result.data.key,
         button_class: 'mediaLibrary',
         button_caption: 'Select Image or Video',
-        cloud_name: result.cloud,
-        // signature: result.signature,
-        // timestamp: result.timestamp,
-        // username: 'accounts@bugo.io',
+        cloud_name: result.data.cloud,
+        signature: result.data.signature,
+        timestamp: result.data.timestamp,
+        username: 'accounts@bugo.io',
       }, {insertHandler: (data) => {
             data.assets.forEach(asset => { 
               console.log(asset)
@@ -97,7 +97,6 @@ export default class Gallery extends Component {
     const { media } = this.state
     console.log('BugoCloudinary Mounted');
     this.getCloudinaryHash()
-    console.log(ML);
   }
 
   render() {

f8994ceea58c7cbebe3a844282d8914094c5c093
Matt Antone <matt.antone@gmail.com>
2020-03-18 13:22:25 -0700

    updated JS

--- src/lib/components/Gallery.js
+++ src/lib/components/Gallery.js
@@ -44,15 +44,17 @@ export default class Gallery extends Component {
       .then((result)=>{
         console.log("yo",result,window.ML);
         if(typeof(result.data) !== 'undefined'){
-          window.ML = window.cloudinary.createMediaLibrary({
-            api_key: result.data.key,
-            button_class: 'mediaLibrary',
-            button_caption: 'Select Image or Video',
-            cloud_name: result.data.cloud,
-            signature: result.data.signature,
-            timestamp: result.data.timestamp,
-            username: 'accounts@bugo.io',
-          }, {insertHandler: (data) => {
+          window.ML = window.cloudinary.createMediaLibrary(
+            {
+              api_key: result.data.key,
+              button_class: 'mediaLibrary',
+              button_caption: 'Select Image or Video',
+              cloud_name: result.data.cloud,
+              signature: result.data.signature,
+              timestamp: result.data.timestamp,
+              username: 'accounts@bugo.io',
+            }, 
+            {insertHandler: (data) => {
                 data.assets.forEach(asset => { 
                   console.log(asset)
                   if(!asset.context){
@@ -64,8 +66,8 @@ export default class Gallery extends Component {
                   this.handleChange()
                 })
               }
-              },
-              document.getElementById("cloudinary-btn")
+            },
+            document.getElementById("cloudinary-btn")
           )
         }
       })
@@ -106,10 +108,10 @@ export default class Gallery extends Component {
     return h('div', {
       style: wrapperStyle,
     },[
-      h('button', {
+      h('span', {
         id: "cloudinary-btn",
         className: 'mediaLibrary',
-      }, 'Loading...'),
+      }, 'Loading Media Library...'),
       // h(Image,{
       //   cloudName: cloudName,
       //   publicId: image.public_id,
